68666087ec98d760bac1f0d52d2c0d98d26d59f0
Petr <bojko.petr2006@gmail.com>
2024-12-15 14:15:26 +0100

    Add project files.

---
+++ Lekce-8-HW/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lekce_8_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
